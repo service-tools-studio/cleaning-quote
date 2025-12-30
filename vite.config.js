d7cecf40d4846849dc0b6e367371c8871742520e
jasmintrotter <jasmintmail@gmail.com>
2025-12-30 00:23:13 -0800

    update url

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: "/cleaning/quote/",
+  base: "/cleaning-quote/",
   plugins: [react(), tailwindcss()],
 });
